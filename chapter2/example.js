de026f3c6bbb7d4e839138fa263ec51cbac9e85d
Gustavo Wu <zoejimmyandadrian@live.com>
2020-02-18 16:10:41 +0800

    test to github

---
+++ chapter2/example.js
@@ -0,0 +1,37 @@
+//变量
+// var mood = 'happy',
+//     age = 33;
+// var MOOD = 'sed';
+// alert(mood);
+// alert(age);
+// alert(MOOD);
+// var height = "about 5'10\"tall";
+// alert(height);
+
+//数组
+// var beatles = Array(4);
+// beatles[0]  = "John";
+// beatles[1] = "Paul";
+// beatles[2] = "George";
+// beatles[3] = "Ringo";
+
+// var beatles = Array("John","Paul","George","Ringo");
+// for (const key in beatles) {
+//     alert(beatles[key]);
+// }
+
+// for (let index = 0; index < beatles.length; index++) {
+//     const element = beatles[index];
+//     alert(element); 
+// }
+
+//对象 写法一
+// var lennon = Object();
+// lennon.name = "John";
+// lennon.age = 1940;
+// lennon.living = false;
+//对象写法2
+var lennon = {name:"John",age:"1940",living:false};
+
+alert(lennon);
+alert(lennon.name);
