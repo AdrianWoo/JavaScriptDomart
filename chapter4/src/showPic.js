3acb222b8d59700bba8a3bacbca9479806f60550
Gustavo Wu <747218766@qq.com>
2020-02-26 17:30:37 +0800

    The chapter4 Over

--- chapter4/src/showPic.js
+++ chapter4/src/showPic.js
@@ -3,11 +3,23 @@ function showpic(whichpic) {
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
     var text = whichpic.getAttribute("title");
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue = text;
 }
 
 function countBodyChildren(){
     var body_element = document.getElementsByTagName("body")[0];
-    alert(body_element.childNodes.length);
+    // alert(body_element.childNodes.length);
+    // alert(body_element.nodeType);
+    var description = document.getElementById("description");
+    alert(description.firstChild.nodeValue);
 }
 
 window.onload = countBodyChildren;
+
+// 读取第一个节点的方法
+// firstChild
+// 读取最后一个节点的方法
+// lastChild
+// 获取节点的内容
+// nodeValue
